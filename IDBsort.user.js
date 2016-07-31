ae3ef21745af1479e86ed10d20eb853e9e237202
friendly-trenchcoat <tippywigwom@yahoo.com>
2016-07-31 17:53:11 -0500

    updated for new version of JNIDB

--- IDBsort.user.js
+++ IDBsort.user.js
@@ -3,21 +3,19 @@
 // @description  Automatically redirect to the same search sorted by price
 // @author       freindly-trenchcoat
 // @namespace    https://github.com/friendly-trenchcoat
-// @match        http://items.jellyneo.net/?go=item*
+// @match        http://items.jellyneo.net/search/?*
 // ==/UserScript==
 
 var url = window.location.href;
-var sort = url.match(/sortby=([^&]*)/);
+var sort = url.match(/sort=([^&]*)/);
 
-if (sort !== null) { 
-    if (sort[1] != "price"){ 
-        url = url.replace("sortby="+sort[1], "sortby=price"); 
+if (sort !== null) {
+    if (sort[1] != "5"){
+        url = url.replace("sort="+sort[1], "sort=5");
         window.location.replace(url);
-    } 
+    }
 }
-else { 
-    url = url.replace("go=item", "go=item&sortby=price"); 
+else {
+    url = url.replace("search/?", "search/?sort=5&sort_dir=desc&");
     window.location.replace(url);
 }
-
-
